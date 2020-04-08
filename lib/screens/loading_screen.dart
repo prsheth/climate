76abea38c7403177c06e30121328599ac637060a
prsheth <pranayrsheth@gmail.com>
2020-04-08 14:40:15 -0400

    Getting location on Android and iOS asynschronously and also added http package to make API calls to servvices

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:climate/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,25 +8,36 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  /*
+    Lifecycle methods for stateful widget
+      initState()-> initial state, called only once, when state is initialized
+      build()-> when widget is built,called every time when image or text changes, too expensive for code called frequently
+      deactivate()-> when state is destroyed
+   */
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
-    //DECLARE AN ASYNC FUNCTION IN DART
-    //Future is a promise in dart
-    Position position = await Geolocator().getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.bestForNavigation);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    Response response = await get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+      body: Container(margin: EdgeInsets.all(30.0), color: Colors.indigo),
     );
   }
 }
